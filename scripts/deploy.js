ab15a1ffa35c1ad7e79314a10bdc7126196816da
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2022-12-17 02:09:40 +0330

    change name deploy file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
 
-  const Certificate = await ethers.getContractFactory("NFTCollection");
+  const Certificate = await ethers.getContractFactory("Airdrop_NFT_Collection");
   const certificate = await Certificate.deploy("Air Drop NFT Collection","ADR");
 
   await certificate.deployed();
